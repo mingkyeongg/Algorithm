437f7c4730a7693a6bd866daea40926f83c94687
Lee Minkyeong <122078277+mingkyeongg@users.noreply.github.com>
2025-01-13 23:55:45 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+  vector<int> v;
+
+  for (int i = 0; i < 3; i++)
+  {
+    int x;
+    cin >> x;
+    v.push_back(x);
+  }
+  sort(v.begin(), v.end());
+
+  cout << v[1];
+}
